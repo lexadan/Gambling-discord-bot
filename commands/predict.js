cc23baa0715b3a0ec1f8de44d04901e8010bc138
Corentin Renard <corentin.renard@epitech.eu>
2021-03-16 01:58:24 +0100

    Add message reaction to prediction

--- commands/predict.js
+++ commands/predict.js
@@ -16,6 +16,7 @@ async function deleteBet(id) {
 	await redis.del(`bet_msg:${msg_id}`);
 	await redis.del(`bet:${id}`);
 }
+
 async function saveBet(message, predict, props) {
 	let id = uniqid();
 	await redis.hmset(`bet:${id}`, predict);
@@ -25,30 +26,47 @@ async function saveBet(message, predict, props) {
 		await redis.hset(`props:${prop_id}`, 'text', props[i]);
 		await redis.hset(`props:${prop_id}`, 'balance', 0);
 	}
-	let msg = await message.channel.send('test ma couille');
-	await redis.set(`bet_msg:${msg.id}`, id);
-	await redis.hset(`bet:${id}`, 'msg_id', msg.id);
-	//msgCtor(message);
+	msgCtor(id, message);
 }
 
 
-async function msgCtor(message) {
+async function msgCtor(id, message) {
 	let Embed = new Discord.MessageEmbed();
 	Embed.setColor("#0099ff").setTitle("Prediction !");
 
-	let totalBet = await redis.get('totalBet');
-	let question = await redis.get('predictQuestion');
-	Embed.setDescription(question);	
-	let props = await redis.zrange('props', 0, 5);
-	let lenght = await redis.zcount('props', 0, 5);
-	for (let i = 0; i < lenght; i++) {
-		let bet = await redis.get(`prop${i + 1}`);
-		let cote = (bet == 0) ? (0) : (totalBet/bet)
+	let bet = await redis.hgetall(`bet:${id}`);
+	Embed.setDescription(bet.question);	
+	for (let i = 0; i < bet.props_nbr; i++) {
+		let prop_id = await redis.hget(`bet:${id}`, `prop:${i}`);
+		let prop = await redis.hgetall(`props:${prop_id}`);
+		let cote = (prop.balance == 0) ? (0) : (bet.balance/prop.balance)
 		let progressbar = progressBar(0, 1, 15);
-		Embed.addField(`${i + 1}) ${props[i]}`, `${progressbar} ${bet} ${config.bet.name} (1:${cote})`);
+		Embed.addField(`${i + 1}) ${bet.text}`, `${progressbar} ${prop.balance} ${config.bet.name} (1:${cote})`);
 	}
 	let msg = await message.channel.send(Embed);
-	await redis.set('predictMessageID', msg.id);
+	await redis.set(`bet_msg:${msg.id}`, id);
+	await redis.hset(`bet:${id}`, 'msg_id', msg.id);
+	for (let i = 0; i < bet.props_nbr; i++) {
+		switch(i) {
+			case 0:
+				await msg.react('1️⃣');
+				break;
+			case 1:
+				await msg.react('2️⃣');
+				break;
+			case 2:
+				await msg.react('3️⃣');
+				break;
+			case 3:
+				await msg.react('4️⃣');
+				break;
+			case 4:
+				await msg.react('5️⃣');
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 module.exports = {
@@ -71,7 +89,8 @@ module.exports = {
 		saveBet(message, {
 			question: question,
 			props_nbr: args.lenght - 1,
-			author_id: message.author.id
+			author_id: message.author.id,
+			balance: 0
 		}, props);
 		log.redis(`New prediction with question : ${question} and props : [${props}] by ${message.author.username}`);
 	}
